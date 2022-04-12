fe8fdc2f006c18694f7e4121d86b97a1bd6b1580
Erikisak <erik.isak@icloud.com>
2022-04-12 19:53:33 +0200

    PUT added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const names= [
+const persons= [
     { id: 1, name: 'Erik'},
     { id: 2, name: 'Tilda'},
     { id: 3, name: 'Max'},
@@ -13,27 +13,36 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.get('/api/names', (req, res) => {
-    res.send(names);
+app.get('/api/persons', (req, res) => {
+    res.send(persons);
 });
 
-app.get('/api/names/:id', (req, res) => {
-   const name = names.find(c => c.id === parseInt(req.params.id));
-   if (!name) res.status(404).send('No name was found');
-   res.send(name)
-});
+app.post('/api/persons', (req, res) => {
 
-app.post('/api/names', (req, res) => {
     const name = {
-        id: names.length + 1,
+        id: persons.length + 1,
         name: req.body.name
     };
-    names.push(name);
+    persons.push(name);
     res.send(name);
 });
 
+app.put('/api/persons/:id', (req, res) => {
+    const name = persons.find(c => c.id === parseInt(req.params.id));
+   if (!name) res.status(404).send('No name was found');
+
+   name.name = req.body.name;
+   res.send(name);
+});
 
 
+
+
+app.get('/api/persons/:id', (req, res) => {
+   const name = persons.find(c => c.id === parseInt(req.params.id));
+   if (!name) res.status(404).send('No name was found');
+   res.send(name);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
