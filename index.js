e340e0d45ccad3f9848f7ce97afeceee05d2c184
Erikisak <erik.isak@icloud.com>
2022-04-12 17:02:00 +0200

    first GET

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require ('express');
 const app = express();
 
+const names= [
+    { id: 1, name: 'Erik'},
+    { id: 2, name: 'Tilda'},
+    { id: 3, name: 'Max'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world');
+});
+
+app.get('/api/names', (req, res) => {
+    res.send(names);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000...') )
