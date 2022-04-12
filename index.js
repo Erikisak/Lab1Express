f8050579ff8752090cb11fd1c9446716728c7783
Erikisak <erik.isak@icloud.com>
2022-04-12 17:22:03 +0200

    404 message

--- index.js
+++ index.js
@@ -15,4 +15,14 @@ app.get('/api/names', (req, res) => {
     res.send(names);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000...') )
+app.get('/api/names/:id', (req, res) => {
+   const name = names.find(c => c.id === parseInt(req.params.id));
+   if (!name) res.status(404).send('No name was found');
+   res.send(name)
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
